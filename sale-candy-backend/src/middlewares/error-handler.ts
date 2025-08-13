98a8682121d45c7a68f7bac3fa954e6f9dcb5885
Antonio Marcos <almeid.ximenes@gmail.com>
2025-08-13 00:12:55 -0300

    feat: ajustando camada Middlewares > Error  personalizado

--- sale-candy-backend/src/middlewares/error-handler.ts
+++ sale-candy-backend/src/middlewares/error-handler.ts
@@ -3,7 +3,13 @@ import { HttpError } from "../errors/Http-Error";
 
 // aqui faz o tratamento para ver em qual caso o error cai 
 export const errorHandlerMiddleware: ErrorRequestHandler = (error, req, res, next) => {
-    if(error instanceof HttpError) res.status(error.statusCode).json({message: error.message}) // verifica se é da classe personalizada
-    if(error instanceof Error) res.status(500).json({message: error.message}) // verifica se é um erro generico
-    else res.status(500).json({message: "Internal Server Error"}) // se não for nenhum ele manda um erro padrao
+    if (error instanceof HttpError) {
+        return res.status(error.statusCode).json({ message: error.message });
+    }
+
+    if (error instanceof Error) {
+        return res.status(500).json({ message: error.message });
+    }
+
+    return res.status(500).json({ message: "Internal Server Error" });
 }

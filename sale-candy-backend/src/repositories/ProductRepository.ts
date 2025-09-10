a14ddedc59463bb3615e479736bf502938c72213
Antonio Ximenes <almeid.ximenes@gmail.com>
2025-09-09 23:14:09 -0400

    feat: Criando o repositorio de Product

---
+++ sale-candy-backend/src/repositories/ProductRepository.ts
@@ -0,0 +1,21 @@
+import { Product } from "@prisma/client";
+
+/* 
+*criando os contratos de interfaces para o repositorio e prisma 
+* forma do contrato =  nomeMetodo: () => Promise<objeto ou null>
+*/
+
+export interface ProductDTO{
+    name: string, 
+    description: string,
+    priceProduct: number,
+    stock: number,
+}
+export interface ProductRepository {
+    findAll: () => Promise<Product[]>
+    findById: (id: number) => Promise<Product | null>
+    create: (params: ProductDTO) => Promise<Product>
+    update: (id: number, params: Partial<ProductDTO>) => Promise<Product | null>
+    delete: (id: number) => Promise<Product | null>
+}
+
